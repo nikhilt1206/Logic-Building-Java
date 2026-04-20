6d29351ffc6a7dddf994fe199d614b0ce9f4d3b4
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-04-20 09:03:17 +0530

    Added solutio1 for swapping 2 numbers using temp var

--- src/com/question1/SwapTwoNumbersSolution1.java
+++ src/com/question1/SwapTwoNumbersSolution1.java
@@ -4,7 +4,17 @@ public class SwapTwoNumbersSolution1 {
 
 	public static void main(String[] args) {
 
+		int a=10;
+		int b=20;
+		int temp=0;
 		
+		System.out.println("Before Swapping: ");
+		System.out.println("a : "+a+ " and b: "+b);
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println("After Swapping: ");
+		System.out.println("a : "+a+ " and b: "+b);
 		
 	}
 
