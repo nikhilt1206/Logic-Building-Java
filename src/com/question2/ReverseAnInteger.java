059e8f51e1c1dc361999a4e1e955124f980109c3
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-04-21 19:56:52 +0530

    added code for Reverse an integer

---
+++ src/com/question2/ReverseAnInteger.java
@@ -0,0 +1,28 @@
+package com.question2;
+
+public class ReverseAnInteger {
+
+	public static void main(String[] args) {
+
+		int number = 2147483647;
+		int lastDigit;
+		int reverseNumber=0;
+		
+		while(number!=0) {
+			lastDigit=number%10;
+			if(reverseNumber>Integer.MAX_VALUE/10 ||(reverseNumber==Integer.MAX_VALUE/10 && lastDigit>7)) {
+				System.out.println(0);
+				return;
+			}
+			if(reverseNumber<Integer.MIN_VALUE/10 ||(reverseNumber==Integer.MIN_VALUE/10 && lastDigit<-8)) {
+				System.out.println(0);
+				return;
+			}
+			reverseNumber=reverseNumber*10+lastDigit;
+			number=number/10;
+		}
+		System.out.println("Reversed number is: "+reverseNumber);
+
+	}
+
+}
