265de853a71b881f46a556d34190d9b44c9630c9
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-04-23 20:08:39 +0530

    Added code for Factorial of a number

---
+++ src/com/question4/FactorialOfANumber.java
@@ -0,0 +1,23 @@
+package com.question4;
+
+import java.math.BigInteger;
+
+public class FactorialOfANumber {
+
+	public static void main(String[] args) {
+
+		int number =20;
+//		for(int i=1;i<=number;i++) {
+//			result=result*i;
+//		}
+		//Factorial of a large number - variation 
+		
+		BigInteger result = BigInteger.ONE;
+		for(int i=1;i<=number;i++) {
+			result = result.multiply(BigInteger.valueOf(i));
+		}
+		
+		System.out.println("Factorial of "+number+" is : "+result);
+	}
+
+}
