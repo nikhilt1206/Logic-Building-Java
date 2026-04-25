012b07525635131b6276d498ba136069d7d1efad
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-04-25 21:28:05 +0530

    Added code for Finding length of the last word in a string

---
+++ src/com/question6/LengthOfLastWordInString.java
@@ -0,0 +1,27 @@
+package com.question6;
+
+public class LengthOfLastWordInString {
+
+	public static void main(String[] args) {
+
+		String s = "   Helllo World   ";
+		int count=0;
+		//s=s.trim();
+		//System.out.println(s);
+		
+		char[] inputArray = s.toCharArray();
+		for(int i=inputArray.length-1;i>=0;i--) {
+			if(inputArray[i]!=' ') {
+				count =count+1;
+			}
+			else {
+				if(count>0) {
+					System.out.println(count);
+					break;
+				}
+			}
+		}
+
+	}
+
+}
