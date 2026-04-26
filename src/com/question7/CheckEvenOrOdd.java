d89b3d647104dba3410c86ceb584ab977c53ad15
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-04-26 20:03:19 +0530

    Added code for printing even words of a string

---
+++ src/com/question7/CheckEvenOrOdd.java
@@ -0,0 +1,17 @@
+package com.question7;
+
+public class CheckEvenOrOdd {
+
+	public static void main(String[] args) {
+
+		int number=32;
+		if(number%2==0) {
+			System.out.println("Even");
+		}
+		else {
+			System.out.println("Odd");
+		}
+
+	}
+
+}
---
+++ src/com/question7/PrintLengthOfString.java
@@ -0,0 +1,12 @@
+package com.question7;
+
+public class PrintLengthOfString {
+
+	public static void main(String[] args) {
+
+		String input = "Java";
+		int size = input.length();
+		System.out.println(size);
+	}
+
+}
