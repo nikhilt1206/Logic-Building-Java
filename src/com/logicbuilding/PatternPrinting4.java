124a287ff3b9d5071d4b7324a1b2df504e6e6b66
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-05-04 19:16:09 +0530

    Added code for Pattern Printing 4

---
+++ src/com/logicbuilding/PatternPrinting4.java
@@ -0,0 +1,21 @@
+package com.logicbuilding;
+
+public class PatternPrinting4 {
+
+	public static void main(String[] args) {
+
+		int totalNumberOfRows = 5;
+		for (int row = 1; row <= totalNumberOfRows; row++) {
+			for (int col = 1; col <= row; col++) {
+				if (col == 1 || col == row || row == totalNumberOfRows) {
+					System.out.print(col);
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
