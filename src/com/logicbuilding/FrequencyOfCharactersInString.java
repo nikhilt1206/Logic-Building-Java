2524524a238d82fc4fe4ec7322085fbfd198cc25
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-05-14 18:55:18 +0530

    Added code for frequency of characters in string

---
+++ src/com/logicbuilding/FrequencyOfCharactersInString.java
@@ -0,0 +1,23 @@
+package com.logicbuilding;
+
+import java.util.HashMap;
+
+public class FrequencyOfCharactersInString {
+
+	public static void main(String[] args) {
+
+		String input = "madam";
+		char charArray[] = input.toCharArray();
+		HashMap<Character,Integer> freqMap = new HashMap<Character,Integer>();
+		
+		for(char c : charArray) {
+			freqMap.put(c, freqMap.getOrDefault(c, 0)+1);
+		}
+		
+		for(char c : freqMap.keySet()) {
+			System.out.println(c + " -> " + freqMap.get(c));
+		}
+
+	}
+
+}
