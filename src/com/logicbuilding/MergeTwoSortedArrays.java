fbfb3f2cdedb46c15fbc8af997c6ca91dd59e957
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-06-02 16:49:13 +0530

    Added code for Merge Two Sorted Arrays

---
+++ src/com/logicbuilding/MergeTwoSortedArrays.java
@@ -0,0 +1,41 @@
+package com.logicbuilding;
+
+import java.util.Arrays;
+
+public class MergeTwoSortedArrays {
+
+	public static void main(String[] args) {
+
+		int a1[] = {1,2,4,6};
+		int a2[] = {3,5,8};
+		int a3[] = new int[a1.length+a2.length];
+		int p1=0;
+		int p2=0;
+		int p3=0;
+		
+		while(p1<a1.length && p2<a2.length) {
+			if(a1[p1]<a2[p2]) {
+				a3[p3]=a1[p1];
+				p3++;
+				p1++;
+			}
+			else {
+				a3[p3]=a2[p2];
+				p3++;
+				p2++;
+			}
+		}
+		while(p1<a1.length) {
+			a3[p3]=a1[p1];
+			p3++;
+			p1++;
+		}
+		while(p2<a2.length) {
+			a3[p3]=a2[p2];
+			p3++;
+			p2++;
+		}
+		System.out.println(Arrays.toString(a3));
+	}
+
+}
