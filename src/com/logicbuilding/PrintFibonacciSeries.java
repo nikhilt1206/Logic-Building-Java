0b71b6edf07c1434c1181f5420419d1d3563ddd3
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-06-06 08:28:56 +0530

    Updated Fibonacci Code

--- src/com/logicbuilding/PrintFibonacciSeries.java
+++ src/com/logicbuilding/PrintFibonacciSeries.java
@@ -4,20 +4,20 @@ public class PrintFibonacciSeries {
 
 	public static void main(String[] args) {
 
-		int num=10;
+		int num = 10;
 		int first = 0;
 		int second = 1;
 		int next;
-		if(num==1) {
+		if (num == 1) {
 			System.out.println(first);
-			System.exit(0);
-		}
-		System.out.print(first+" "+second+" ");
-		for(int i=2;i<num;i++) {
-			next=first+second;
-			System.out.print(next+" ");
-			first=second;
-			second=next;
+		} else {
+			System.out.print(first + " " + second + " ");
+			for (int i = 2; i < num; i++) {
+				next = first + second;
+				System.out.print(next + " ");
+				first = second;
+				second = next;
+			}
 		}
 	}
 
