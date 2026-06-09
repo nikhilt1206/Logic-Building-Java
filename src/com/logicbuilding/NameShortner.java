1a255d7b56cd46642c738becb144dbfb5cd6f22f
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-06-09 08:34:16 +0530

    Added code for Name Shortner Program

---
+++ src/com/logicbuilding/NameShortner.java
@@ -0,0 +1,24 @@
+package com.logicbuilding;
+
+public class NameShortner {
+
+	public static void main(String[] args) {
+
+		String fullName = "Vidya Bhushan Tiwari";
+		String nameParts[] = fullName.trim().split(" ");
+		if (nameParts.length < 2) {
+			System.out.println(fullName);
+		} 
+		else 
+		{
+			StringBuilder sb = new StringBuilder();
+
+			for (int index = 0; index < nameParts.length - 1; index++) {
+				sb.append(nameParts[index].charAt(0));
+				sb.append(". ");
+			}
+			sb.append(nameParts[nameParts.length - 1]);
+			System.out.println(sb);
+		}
+	}
+}
