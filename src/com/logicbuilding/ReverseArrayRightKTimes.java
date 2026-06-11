c460d4e0d999457bb13d1dda6dbc8dd86b17f4b1
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-06-11 08:07:54 +0530

    Added code for Array Rotation by right

---
+++ src/com/logicbuilding/ReverseArrayRightKTimes.java
@@ -0,0 +1,35 @@
+package com.logicbuilding;
+
+import java.util.Arrays;
+
+public class ReverseArrayRightKTimes {
+
+	public static void main(String[] args) {
+
+		int a[] = { 1, 2, 3, 4, 5, 6, 7 };
+		int n = 3;
+		int k = n%a.length;
+		int left = 0;
+		int right = a.length - 1;
+		a = reverseArray(a, left, right);
+		System.out.println(Arrays.toString(a));
+		a = reverseArray(a,left,k-1);
+		System.out.println(Arrays.toString(a));
+		a = reverseArray(a,k,right);
+		System.out.println(Arrays.toString(a));
+	}
+
+	private static int[] reverseArray(int[] a, int left, int right) {
+
+		while (left < right) {
+			int temp;
+			temp = a[left];
+			a[left] = a[right];
+			a[right] = temp;
+			left++;
+			right--;
+		}
+		return a;
+	}
+
+}
