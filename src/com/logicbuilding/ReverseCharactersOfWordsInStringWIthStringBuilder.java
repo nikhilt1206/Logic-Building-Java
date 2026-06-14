c322e34f18a23bc5285deaeec891b8c913e00df1
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-06-14 09:57:36 +0530

    Added code for Reverse Characters of a word in a strinf using StringBuilder .reverse() method

---
+++ src/com/logicbuilding/ReverseCharactersOfWordsInStringWIthStringBuilder.java
@@ -0,0 +1,16 @@
+package com.logicbuilding;
+
+public class ReverseCharactersOfWordsInStringWIthStringBuilder {
+
+	public static void main(String[] args) {
+
+		String input = "This is fun";
+		String[] words = input.split(" ");
+		StringBuilder result = new StringBuilder();
+		for(String word: words) {
+			StringBuilder reverseWord = new StringBuilder(word);
+			result.append(reverseWord.reverse().append(" "));
+		}
+		System.out.println(result.toString().trim());
+	}
+}
