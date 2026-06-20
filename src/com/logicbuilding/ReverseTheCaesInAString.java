79232548a4a9fc85024af9e12a1a53f9f26cc432
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-06-20 10:11:50 +0530

    Added code for Reverse the case in a String

---
+++ src/com/logicbuilding/ReverseTheCaesInAString.java
@@ -0,0 +1,31 @@
+package com.logicbuilding;
+
+public class ReverseTheCaesInAString {
+
+	public static void main(String[] args) {
+
+		String input = "Hello World";
+		
+		if(input==null || input.isEmpty()) {
+			System.out.println("Invalid Input!!");
+			return;
+		}
+		
+		char[] data = input.toCharArray();
+		StringBuilder sb = new StringBuilder();
+		
+		for(char c : data) {
+			if(Character.isUpperCase(c)) {
+				sb.append(Character.toLowerCase(c));
+			}
+			else if(Character.isLowerCase(c)) {
+				sb.append(Character.toUpperCase(c));
+			}
+			else {
+				sb.append(c);
+			}
+		}
+		System.out.println(sb);
+	}
+
+}
