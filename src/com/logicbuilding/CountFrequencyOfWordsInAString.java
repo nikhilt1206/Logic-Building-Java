44ce599cb6ce0c4e626d6d08dccd44849e86f778
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-06-29 10:07:34 +0530

    Adde code for counting the frequency of words in a string

---
+++ src/com/logicbuilding/CountFrequencyOfWordsInAString.java
@@ -0,0 +1,27 @@
+package com.logicbuilding;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CountFrequencyOfWordsInAString {
+
+	public static void main(String[] args) {
+
+		String input ="Hi my name is Rohan Rohan";
+		if(input==null || input.isEmpty()) {
+			System.out.println("Invalid input!!");
+			return;
+		}
+		String words[] = input.split(" ");
+		LinkedHashMap<String,Integer> map = new LinkedHashMap<String,Integer>();
+		
+		for(String s : words) {
+			map.put(s, map.getOrDefault(s, 0)+1);
+		}
+		
+		for(Map.Entry<String, Integer> data : map.entrySet()) {
+			System.out.println(data.getKey()+"->"+data.getValue());
+		}	
+	}
+
+}
