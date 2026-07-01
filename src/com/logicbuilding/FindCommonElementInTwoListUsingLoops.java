d5d3c0fcd982258bedd2ed48b7d803f5afd4cfdd
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-07-01 09:47:23 +0530

    Added code for finding common element in 2 list using loop

---
+++ src/com/logicbuilding/FindCommonElementInTwoListUsingLoops.java
@@ -0,0 +1,34 @@
+package com.logicbuilding;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindCommonElementInTwoListUsingLoops {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> list1 = new ArrayList<String>();
+		list1.add("One");
+		list1.add("How");
+		list1.add("Ten");
+		
+		ArrayList<String> list2 = new ArrayList<String>();
+		list2.add("Two");
+		list2.add("How");
+		list2.add("One");
+		list2.add("One");
+		
+		Set<String> result = new HashSet<String>();
+		
+		for(int i=0;i<list1.size();i++) {
+			for(int j=0;j<list2.size();j++) {
+				if(list1.get(i).equalsIgnoreCase(list2.get(j))) {
+					result.add(list1.get(i));
+				}
+			}
+		}
+		System.out.println(result);
+	}
+
+}
