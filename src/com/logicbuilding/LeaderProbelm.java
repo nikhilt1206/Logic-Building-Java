52c176c884a0789912844068fef316c331413cc5
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-07-05 11:23:30 +0530

    Updated code for leader number in array

--- src/com/logicbuilding/LeaderProbelm.java
+++ src/com/logicbuilding/LeaderProbelm.java
@@ -1,5 +1,9 @@
 package com.logicbuilding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class LeaderProbelm {
 
 	public static void main(String[] args) {
@@ -8,14 +12,16 @@ public class LeaderProbelm {
 	
 		int length = a.length;
 		int maxRight=a[length-1];
-		
-		System.out.print(maxRight+" ");
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(maxRight);
 		
 		for(int index=length-2;index>=0;index--) {
 			if(a[index]>maxRight) {
 				maxRight=a[index];
-				System.out.print(maxRight+" ");
+				list.add(maxRight);
 			}
 		}
+		Collections.reverse(list);
+		System.out.println(list);
 	}
 }
