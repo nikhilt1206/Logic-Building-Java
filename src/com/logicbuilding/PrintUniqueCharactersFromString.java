15ac1f8a85d8d07399823527dedfda0e9876225d
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-07-06 09:44:05 +0530

    Added code for finding unique characters from a String

---
+++ src/com/logicbuilding/PrintUniqueCharactersFromString.java
@@ -0,0 +1,28 @@
+package com.logicbuilding;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class PrintUniqueCharactersFromString {
+
+	public static void main(String[] args) {
+
+		String input = "abbacdaab";
+		if(input==null || input.isEmpty()) {
+			System.out.println("Invalid input!!");
+			return;
+		}
+		
+		Set<Character> set = new LinkedHashSet<Character>();
+		StringBuilder sb = new StringBuilder();
+		
+		for(char c : input.toCharArray()) {
+			set.add(c);
+		}
+		for(char c : set) {
+			sb.append(c);
+		}
+		System.out.println(sb);
+	}
+
+}
