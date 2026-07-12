ad6cf5a9a6b9ac0a7c280e2fa361c7b27c2d0239
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-07-12 13:00:19 +0530

    Updated String Decoding code

--- src/com/logicbuilding/StringDecoding.java
+++ src/com/logicbuilding/StringDecoding.java
@@ -5,17 +5,21 @@ public class StringDecoding {
 	public static void main(String[] args) {
 
 		String input = "d3m4t2b0a1";
-		if(input==null || input.isEmpty()) {
+		if (input == null || input.isEmpty()) {
 			System.out.println("Invalid input!!");
 			return;
 		}
 		StringBuilder sb = new StringBuilder();
-		
-		for(int i=0;i<input.length();i=i+2) {
-			char letter = input.charAt(i);
-			int count = Character.getNumericValue(input.charAt(i+1));
-			for(int j=0;j<count;j++) {
-				sb.append(letter);
+		char currentChar = ' ';
+
+		for (int i = 0; i < input.length(); i++) {
+			if (Character.isLetter(input.charAt(i))) {
+				currentChar = input.charAt(i);
+			} else if (Character.isDigit(input.charAt(i))) {
+				int count = Character.getNumericValue(input.charAt(i));
+				for (int j = 0; j < count; j++) {
+					sb.append(currentChar);
+				}
 			}
 		}
 		System.out.println(sb);
