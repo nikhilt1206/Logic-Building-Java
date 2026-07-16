0d55ed5db4d658a74b3630b6d9b39f283a1d724a
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-07-16 09:07:08 +0530

    Updated code for remove all whitespaces from a string

--- src/com/logicbuilding/RemoveAllExtraWhiteSpaces.java
+++ src/com/logicbuilding/RemoveAllExtraWhiteSpaces.java
@@ -5,6 +5,10 @@ public class RemoveAllExtraWhiteSpaces {
 	public static void main(String[] args) {
 
 		String input = "   Hello    World   ";
+		if(input==null||input.isEmpty()) {
+			System.out.println("Invalid input!!");
+			return;
+		}
 		input = input.trim();
 		input = input.replaceAll("\\s+", " ");
 		System.out.println(input);
