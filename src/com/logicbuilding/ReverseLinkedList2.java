4f78771837646834fb4542b325347869e56c976d
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-08-05 11:02:43 +0530

    Linked List Reversal

---
+++ src/com/logicbuilding/ReverseLinkedList2.java
@@ -0,0 +1,38 @@
+package com.logicbuilding;
+
+public class ReverseLinkedList2 {
+
+	public static void main(String[] args) {
+
+		Node head = new Node(10);
+		head.next = new Node(20);
+		head.next.next = new Node(30);
+		head.next.next.next = new Node(40);
+		
+		traverseLinkedList(head);
+		traverseLinkedList(reverseLinkedList(head));
+	}
+
+	public static void traverseLinkedList(Node head) {
+		System.out.println("");
+		Node current = head;
+		while(current!=null) {
+			System.out.print(current.getData()+" ");
+			current = current.next;
+		}
+	}
+	
+	public static Node reverseLinkedList(Node head) {
+		Node current = head;
+		Node prev = null;
+		Node next = null;
+		
+		while(current!=null) {
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+		}
+		return prev;
+	}
+}
